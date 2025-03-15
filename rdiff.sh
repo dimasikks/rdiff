f38eeb0ccae4b91bd0596cb48b17e8234320e126
dimasikks <optipro38@gmail.com>
2025-03-15 16:38:54 +0300

    RDIFF - added full remote ability

--- rdiff.sh
+++ rdiff.sh
@@ -2,25 +2,85 @@
 
 if [ $(echo $@ | grep '.*\@.*\:.*' | wc -l) -eq 0 ]; then
 	diff $@
+	echo 123
 	exit 0
 fi
 
 ON='\033[0;33m'
 OFF='\033[0m'
 
-local_path=$1
+type=''
 
-input=($(echo $2 | tr '@' ' ' | tr ':' ' '))
-user=${input[0]}
-host=${input[1]}
-remote_path=${input[2]}
+output_msg () {
+	out_1=$(echo "$3" | grep '^<' | awk '{print $2}' | awk '/^$/ {print "[EMPTY FIELD]"; next} {print}')
+	out_2=$(echo "$3" | grep '^>' | awk '{print $2}' | awk '/^$/ {print "[EMPTY FIELD]"; next} {print}')
+	
+	result=''
+	if [ -z "$out_1" ]; then
+		result='None'
+	fi
+	echo -e "\n${ON}Differences that exist in $1:${OFF} $result\n$out_1"
+	
+	result=''
+	if [ -z "$out_2" ]; then
+		result='None'
+	fi
+	echo -e "\n${ON}Differences that exist in $2:${OFF} $result\n$out_2"
+}
 
-output=$(ssh -o StrictHostKeyChecking=no -q "$user@$host" cat "$remote_path" | diff "$local_path" -)
+choose_diff() {
+	type=$3
 
-echo -e "\n${ON}Differences that exist in local:${OFF}\n"
-echo "$output" | grep '^<' | awk '{print $2}'
+	input_1=($(echo $1 | tr '@' ' ' | tr ':' ' '))
+	input_2=($(echo $2 | tr '@' ' ' | tr ':' ' '))
 
-echo -e "\n${ON}Differences that exist in remote:${OFF}\n"
-echo "$output" | grep '^>' | awk '{print $2}'
+	if [ "$type" == "both" ]; then
+		user_1=${input_1[0]}
+		host_1=${input_1[1]}
+		remote_path_1=${input_1[2]}
 
+		user_2=${input_2[0]}
+		host_2=${input_2[1]}
+		remote_path_2=${input_2[2]}
 
+		started=$(ssh -o StrictHostKeyChecking=no -q "$user_2@$host_2" cat "$remote_path_2")
+		output=$(echo -e "$started" | diff - <(ssh -o StrictHostKeyChecking=no -q "$user_1@$host_1" cat "$remote_path_1"))
+
+		output_msg "$host_1:$remote_path_1" "$host_2:$remote_path_2" "$output"
+	fi
+
+	if [ "$type" == "left" ]; then
+		user=${input_1[0]}
+		host=${input_1[1]}
+		remote_path=${input_1[2]}
+
+		local_path=${input_2[0]}
+
+		output=$(cat "$local_path" | diff - <(ssh -o StrictHostKeyChecking=no -q "$user@$host" cat "$remote_path"))
+
+		output_msg "$host:$remote_path" "$local_path" "$output"
+	fi
+
+	if [ "$type" == "right" ]; then
+		user=${input_2[0]}
+		host=${input_2[1]}
+		remote_path=${input_2[2]}
+
+		local_path=${input_1[0]}
+
+		output=$(ssh -o StrictHostKeyChecking=no -q "$user@$host" cat "$remote_path" | diff "$local_path" -)
+
+		output_msg "$local_path" "$host:$remote_path" "$output"
+	fi
+}
+
+res_1=$(echo $1 | grep '.*\@.*\:.*' | wc -l)
+res_2=$(echo $2 | grep '.*\@.*\:.*' | wc -l)
+
+if [[ ( $res_1 -eq 1) && ( $res_2 -eq 1) ]]; then
+	choose_diff $1 $2 "both"
+elif [ $res_1 -eq 1 ]; then
+	choose_diff $1 $2 "left"
+elif [ $res_2 -eq 1 ]; then
+	choose_diff $1 $2 "right"
+fi
